3000cbe06ec304d5ab85551b9a028206a769e231
adc_100004713566_rocknio <adc_100004713566_rocknio@git.cloud.tencent.com>
2020-02-20 15:33:30 +0800

    1、碰撞检测，子弹击中敌机效果 2、敌机向下运动 3、敌机击中后重新生成新的敌机

--- flight.py
+++ flight.py
@@ -3,100 +3,102 @@
 import pygame
 import time
 from pygame.locals import *
+import random
 
 screen_width = 480
 screen_height = 852
 
 
-class Hero(object):
-    def __init__(self, screen):
-        self.x = 190
-        self.y = 740
+class Hero(pygame.sprite.Sprite):
+    def __init__(self, screen, *groups):
+        super().__init__(*groups)
         self.width = 100
         self.height = 110
         self.screen = screen
-        self.image = pygame.image.load("./resources/flight-1.png")
+        self.image = pygame.image.load("./resources/flight-1.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
-        self.bullets = []
+        self.rect = self.image.get_rect()
+        self.rect.x = 190
+        self.rect.y = 740
+        self.bullets = pygame.sprite.Group()
 
     def display_hero(self):
-        self.screen.blit(self.image, (self.x, self.y))
+        self.screen.blit(self.image, (self.rect.x, self.rect.y))
+        self.bullets.draw(self.screen)
         for bullet in self.bullets:
-            bullet.display()
             bullet.move()
             if bullet.judge():
                 self.bullets.remove(bullet)
 
     def move_left(self):
-        if self.x > 0:
-            self.x -= 5
+        if self.rect.x > 0:
+            self.rect.x -= 5
 
     def move_right(self):
-        if self.x < (screen_width - self.width):
-            self.x += 5
+        if self.rect.x < (screen_width - self.width):
+            self.rect.x += 5
 
     def move_up(self):
-        if self.y > self.height:
-            self.y -= 5
+        if self.rect.y > self.height:
+            self.rect.y -= 5
 
     def move_down(self):
-        if self.y < (screen_height - self.height):
-            self.y += 5
+        if self.rect.y < (screen_height - self.height):
+            self.rect.y += 5
 
     def fire(self):
-        self.bullets.append(Bullet(self.screen, self.x, self.y))
+        self.bullets.add(Bullet(self.screen, self.rect.x, self.rect.y))
 
 
-class Bullet(object):
-    def __init__(self, screen, x, y):
-        self.x = x + 40
-        self.y = y
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y, *groups):
+        super().__init__(*groups)
+        # self.x = x + 40
+        # self.y = y
         self.width = 15
         self.height = 15
         self.screen = screen
-        self.image = pygame.image.load("./resources/bullet.png")
+        self.image = pygame.image.load("./resources/bullet.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x + 40
+        self.rect.y = y
 
     def display(self):
-        self.screen.blit(self.image, (self.x, self.y - 20))
+        self.screen.blit(self.image, (self.rect.x, self.rect.y - 20))
 
     def move(self):
-        self.y -= 5
+        self.rect.y -= 5
 
     def judge(self):
-        if self.y <= 0:
+        if self.rect.y <= 0:
             return True
         else:
             return False
 
 
-class Enemy(object):
-    def __init__(self, screen):
-        self.x = 0
-        self.y = 0
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, screen, *groups):
+        super().__init__(*groups)
+        # self.x = 0
+        # self.y = 0
         self.screen = screen
-        self.image = pygame.image.load("./resources/enemy.png")
+        self.image = pygame.image.load("./resources/enemy.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (100, 100))
         self.image = pygame.transform.rotate(self.image, 180)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, screen_width - self.rect.width)
         self.bullets = []
-        self.direction = "right"
+        self.direction = "down"
 
     def display(self):
-        self.screen.blit(self.image, (self.x, self.y))
+        self.screen.blit(self.image, (self.rect.x, self.rect.y))
 
     def fire(self):
-        self.bullets.append(Bullet(self.screen, self.x, self.y))
+        self.bullets.append(Bullet(self.screen, self.rect.x, self.rect.y))
 
     def move(self):
-        if self.direction == "right":
-            self.x += 5
-        elif self.direction == "left":
-            self.x -= 5
-
-        if self.x > screen_width - 100:
-            self.direction = "left"
-        elif self.x <= 0:
-            self.direction = "right"
+        self.rect.y += 5
 
 
 def key_control(hero):
@@ -110,20 +112,31 @@ def key_control(hero):
 
     key_pressed = pygame.key.get_pressed()
     if key_pressed[K_s] or key_pressed[K_DOWN]:
-        print("down")
+        # print("down")
         hero.move_down()
 
     if key_pressed[K_w] or key_pressed[K_UP]:
-        print("up")
+        # print("up")
         hero.move_up()
 
     if key_pressed[K_a] or key_pressed[K_LEFT]:
-        print("left")
+        # print("left")
         hero.move_left()
 
     if key_pressed[K_d] or key_pressed[K_RIGHT]:
-        print("right")
+        # print("right")
         hero.move_right()
+
+
+def check_collide(hero, enemy):
+    if len(hero.bullets) == 0:
+        return False
+
+    collide_list = pygame.sprite.spritecollide(enemy, hero.bullets, True)
+    if len(collide_list) > 0:
+        return True
+    else:
+        return False
 
 
 def main():
@@ -145,6 +158,9 @@ def main():
 
         enemy.display()
         enemy.move()
+
+        if check_collide(hero, enemy):
+            enemy = Enemy(screen)
 
         key_control(hero)
 
